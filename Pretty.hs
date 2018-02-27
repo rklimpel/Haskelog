b96190662eaf2ab6dc4ea2b39ce113f0f974d165
Rico Klimpel <rico@tklimpel.de>
2018-02-27 15:07:50 +0100

    Stringify the little terms

--- Pretty.hs
+++ Pretty.hs
@@ -6,11 +6,46 @@ class Pretty a where
     pretty :: a -> String
 
 instance Pretty Term where
-    pretty (Comb "append" terms) = "append(" ++ (pretty terms) ++ ")"
-    pretty (Comb "." terms)      = "." ++ (pretty terms)
-    pretty (Comb s terms)        = s ++ (pretty terms)
-    pretty (Var x)               = "var " ++ (show x)   
 
-instance Pretty [a] where
-    pretty [a] = ".A"
-    pretty (a:as) = ".AS"
+    pretty (Var x)               = "Var " ++ (show x)
+    pretty (Comb s [])           = s
+    pretty (Comb "." terms)      = dot terms
+    pretty (Comb "append" terms) = append terms
+    pretty (Comb s [t])          = pretty t
+    pretty (Comb s (t:ts))       = listToString (map pretty (t:ts))
+
+
+
+listToString :: [String] -> String
+listToString [x]    = x
+listToString (x:xs) = x ++ "," ++ (listToString xs)
+
+
+append :: [Term] -> String 
+append t = "append(" ++ listToString(map pretty t) ++ ")"
+
+dot :: [Term] -> String
+dot [t] = pretty t
+dot (t:ts) = let a = (pretty t)
+                 b = (pretty (head ts))
+             in if (isEmpty b) then "[" ++ a ++ "]"
+                else if (isList b) then  "[" ++ a ++ "," ++ (removeBrackets b) ++ "]"
+                else "[" ++ a ++ "," ++ b ++ "]"
+
+
+isEmpty :: String -> Bool 
+isEmpty s
+    | s == "[]" = True
+    | otherwise = False
+
+removeBrackets :: String -> String 
+removeBrackets (_:xs) = reverse (removeHead (reverse xs))
+
+removeHead :: String -> String
+removeHead (_:xs) = xs
+
+isList :: String -> Bool
+isList [x] = False
+isList (x:xs) 
+    | x == '[' = True
+    | otherwise = False
--- Test.hs
+++ Test.hs
@@ -4,4 +4,13 @@ import Type
 import Pretty
 
 sample :: Term
-sample = Comb "append" [Var 0,Comb "." [Var 1, Var 2], Comb "." [Comb "2" [], Comb "[]" []]]
+sample = Comb "append" [ Var 0, Comb "." [ Var 1, Var 2 ], Comb "." [ Comb "1" [], Comb "." [ Comb "2" [], Comb "[]" [] ]]]
+-- Var 0, [Var 1, Var 2] , [1,[2,()]]
+-- "append(A, [B|C], [1,2])""
+
+
+sample2 :: Term
+sample2 = Comb "." [Var 0,Var 1]
+-- "[A,B]"
+
+-- Var 0,[Var 1,Var 2],[1,[2,[]]]
