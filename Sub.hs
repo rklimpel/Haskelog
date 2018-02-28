0ae2294ecba19446c97bdec7fce3666c5f7e1ac6
Rico Klimpel <rico@tklimpel.de>
2018-02-28 16:36:02 +0100

    subst compose working fine :-)

--- Sub.hs
+++ Sub.hs
@@ -27,8 +27,31 @@ apply (Subst ((Replace i t):rs)) (Var v)      = if v == i then t else apply (Sub
 -- Term ist 'noch' komplizierter
 apply sub (Comb s terms)                      = (Comb s (map (apply sub) terms))
 
-
 -- komponiert zwei Substitutionen
 -- Remember: apply(compse s2 s1) t == apply s2 (apply s1 t)
 compose :: Subst -> Subst -> Subst
-compose _ _ = Subst []
+
+-- Eine der Substitutionen ist leer
+compose s1 (Subst [])           = s1
+compose (Subst []) s2           = s2
+
+-- Wende Subst1 auf Terme von Subst2 an
+compose (Subst r1s) (Subst r2s) = 
+    Subst ((buildReplace (map getIndex r2s) (map (apply (Subst r1s)) (map getTerm r2s))) ++ r1s)
+
+-- get VarIndex from Replacement
+getIndex :: Replace -> VarIndex
+getIndex (Replace i t) = i
+
+-- get Term from Replacement
+getTerm ::  Replace -> Term
+getTerm (Replace i t) = t
+
+-- Build [Replace] form [VarIndex] and [Term]
+buildReplace :: [VarIndex] -> [Term] -> [Replace]
+buildReplace is ts = buildReplaceH is ts []
+
+-- Helper for buildReplace
+buildReplaceH :: [VarIndex] -> [Term] -> [Replace] -> [Replace]
+buildReplaceH [i] [t] r         = r ++ [(Replace i t)]
+buildReplaceH (i:is) (t:ts) r   =  [(Replace i t)] ++ (buildReplaceH is ts r)
