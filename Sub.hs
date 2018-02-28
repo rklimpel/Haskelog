47b7d17d2017bb7e726a413536ea806037fe0414
rklimpel <rico@tklimpel.de>
2018-02-28 20:48:33 +0100

    rearranged sub.hs code

--- Sub.hs
+++ Sub.hs
@@ -11,34 +11,31 @@ empty = Subst []
 single :: VarIndex -> Term -> Subst
 single var term = Subst [(Replace var term)]
 
+
 -- wendet Substitution auf einen Term an
 apply :: Subst -> Term -> Term
-
 -- Subst leer
 apply (Subst []) t                            = t
-
 -- Term ist konstante
 apply _ (Comb c [])                           = (Comb c [])
-
 -- Term ist eine Variable
 -- apply (Subst [(Replace i t)]) (Var v)         = if v == i then t else (Var v)
 apply (Subst ((Replace i t):rs)) (Var v)      = if v == i then t else apply (Subst rs) (Var v)
-
 -- Term ist 'noch' komplizierter
 apply sub (Comb s terms)                      = (Comb s (map (apply sub) terms))
+
 
 -- komponiert zwei Substitutionen
 -- Remember: apply(compse s2 s1) t == apply s2 (apply s1 t)
 compose :: Subst -> Subst -> Subst
-
 -- Eine der Substitutionen ist leer
 compose s1 (Subst [])           = s1
 compose (Subst []) s2           = s2
-
 -- Wende Subst1 auf Terme von Subst2 an
 compose (Subst r1s) (Subst r2s) = 
     Subst ((buildReplace (map getIndex r2s) (map (apply (Subst r1s)) (map getTerm r2s))) ++ r1s)
 
+    
 -- get VarIndex from Replacement
 getIndex :: Replace -> VarIndex
 getIndex (Replace i t) = i
