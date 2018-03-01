b5b2c3e22fe6cac516585b84e3126250d3fb00b3
rklimpel <rico@tklimpel.de>
2018-03-02 00:32:10 +0100

    log sad SLD progress :sad:

--- SLDTree.hs
+++ SLDTree.hs
@@ -5,4 +5,4 @@ import Type
 -- konstruiert den SLD-Baum zu einem Programm und einer Anfrage
 -- Selektionsstrategie FIRST (es wird immer das linkeste Literal zum Beweisen ausgewählt)
 sld :: Prog -> Goal -> SLDTree
-
+sld _ _ = SLDTree (Goal []) []
