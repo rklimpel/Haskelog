901e99972d42d975d21ae377c9e6a37c649ef00f
Rico Klimpel <rico@tklimpel.de>
2018-03-01 09:33:19 +0100

    create Utils Folder with String & Term Utils outsourced

--- Pretty.hs
+++ Pretty.hs
@@ -1,6 +1,7 @@
 module Pretty where 
 
 import Type
+import Utils.StringUtils
 
 class Pretty a where 
     pretty :: a -> String
@@ -29,37 +30,8 @@ dot (Comb c1 []) (Comb c2 [])      = "[" ++ c1 ++ "," ++ c2 ++ "]"
 dot (Comb c []) (Comb "." (t1:t2)) = "[" ++ c ++ "," ++ (removeBrackets (dot t1 (head t2))) ++ "]"
 
 -- hält das Gesamte Alphabet von A-Z als liste von chars
-alphabet :: [Char]
+alphabet :: String
 alphabet = ['A'..'Z']
 
--- wandelt einenen einzelnen char in eine einelementige liste von chars um (-> String)
-charToString :: Char -> String
-charToString c = [c]
-
--- wandelt eine Liste von Strings in einen einzelnen String um (& setzt Kommas)
-listToString :: [String] -> String
-listToString [x]    = x
-listToString (x:xs) = x ++ "," ++ (listToString xs)
-
--- entfernt Ecke Klammern am anfang und Ende eines Strings
-removeBrackets :: String -> String 
-removeBrackets (_:xs) = reverse (removeHead (reverse xs))
-
--- entfernt das erste Element eines Strings
-removeHead :: String -> String
-removeHead (_:xs) = xs
-
--- gibt True zurück wenn ein String folgende gestalt hat: "[]"
-isEmpty :: String -> Bool 
-isEmpty s
-    | s == "[]" = True
-    | otherwise = False
-
--- gibt True zrück wenn ein String mit "[" beginnt 
-isList :: String -> Bool
-isList [x] = False
-isList (x:xs) 
-    | x == '[' = True
-    | otherwise = False
 
 
---
+++ Utils/TermUtils.hs
@@ -0,0 +1,30 @@
+module Utils.TermUtils where
+
+import Type
+import Pretty
+
+-- True if given Term is Var
+isVar :: Term -> Bool
+isVar (Var i)    = True
+isVar (Comb s t) = False
+
+-- check if Term1 contains Term2 (Term 2 is a subterm of Term1)
+containsSubterm :: Term -> Term -> Bool
+containsSubterm (Var x) (Var y)
+    | x == y                            = True
+    | otherwise                         = False
+containsSubterm (Var x) (Comb s t)      = False
+containsSubterm (Comb s []) t
+    | pretty (Comb s []) == pretty t    = True
+    | otherwise                         = False
+containsSubterm (Comb s [t1]) t2
+    | pretty (Comb s [t1]) == pretty t2 = True
+    | otherwise                         = containsSubterm t1 t2
+containsSubterm (Comb s (t1:t1s)) t2s   = containsSubtermL
+
+-- check if a list of Terms contains a subterm
+containsSubtermL :: [Term] -> Term -> Bool
+containsSubtermL [t1] t2            = containsSubterm t1 t2
+containsSubtermL (t1:t1s) t2
+    | containsSubterm t1 t2 == True = True
+    | otherwise                     = containsSubtermL t1s t2
