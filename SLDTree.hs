972c6baefe619aa8e1c7db12f0a361d0e6791769
Rico Klimpel <rico@tklimpel.de>
2018-03-02 08:59:37 +0100

    write get Vars in (..) funcs for SLD

--- SLDTree.hs
+++ SLDTree.hs
@@ -1,4 +1,4 @@
-module SLDTree where
+module SLDTree (sld) where
 
 import Type
 
@@ -6,3 +6,26 @@ import Type
 -- Selektionsstrategie FIRST (es wird immer das linkeste Literal zum Beweisen ausgewählt)
 sld :: Prog -> Goal -> SLDTree
 sld _ _ = SLDTree (Goal []) []
+
+
+
+
+-- gibt eine Liste an Variablen zurück die in einem Term Vorkommen                             
+getVarsInTerm :: Term -> [VarIndex]
+getVarsInTerm (Var a)     = [a]
+getVarsInTerm (Comb _ []) = []
+getVarsInTerm (Comb _ ts) = foldr (++) [] (map getVarIndex ts)
+
+-- gibt alle Varibalen zurück die in einer Anfrage vorkommen
+getVarsInGoal :: Goal -> [VarIndex]
+getVarsInGoal (Goal gs) = nub (varsInTermList gs)
+
+-- gibt alle Variablen zurück die in einer Regel vorkommen
+getVarsInRule :: Rule -> [Int]
+getVarsInRule (rl :- rr) = varsInTermList ([rl] ++ rr)
+
+-- gibt alle Variablen zurück die in einer Liste von Termn vorkommen
+getVarsinTermlist :: [Term] -> [Int]
+getVarsinTermlist []                 = []
+getVarsinTermlist ((Var x) : xs)     = [x] ++ (varsInTermList xs)
+getVarsinTermlist ((Comb _ xs) : ts) = (varsInTermList xs) ++ (varsInTermList ts)
