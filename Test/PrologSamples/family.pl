151108a37218f6bfdb81b538fd365d78d60a0fba
rklimpel <rico@tklimpel.de>
2018-03-05 21:08:17 +0100

    bugfixes in Prolog family sample

--- Test/PrologSamples/family.pl
+++ Test/PrologSamples/family.pl
@@ -4,15 +4,19 @@ vater(justin,kobe).
 mutter(olaf,lara).
 mutter(lara,chanti).
 mutter(kobe,anja).
+mutter(chanti,anja).
 
 ehemann(lara,heiner).
 ehemann(chanti,rainer).
-ehemann(anja,franz-josef).
+ehemann(anja,franzjosef).
 
 vater(S,V) :- mutter(S,M),ehemann(M,V).
+
 eltern(K,M) :- mutter(K,M).
 eltern(K,V) :- vater(K,V).
+
 oma(E,O) :- eltern(E,P),mutter(P,O).
 opa(E,O) :- eltern(E,P),vater(P,O).
-uropa(UE,UO) :- eltern(UE,E),eltern(P,G),vater(G,UO).
-uroma(UE,UO) :- eltern(UE,E),eltern(P,G),mutter(G,UO).
+
+uropa(UE,UO) :- eltern(UE,E),eltern(E,G),vater(G,UO).
+uroma(UE,UO) :- eltern(UE,E),eltern(E,G),mutter(G,UO).    
