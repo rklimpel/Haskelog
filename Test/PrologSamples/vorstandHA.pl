f4f4b1553713a93050d9b4b4d47bfc5eded53e21
rklimpel <rico@tklimpel.de>
2018-03-05 21:29:15 +0100

    make vorstandHA readable for simpleProlog again

--- Test/PrologSamples/vorstandHA.pl
+++ Test/PrologSamples/vorstandHA.pl
@@ -16,10 +16,7 @@ verschieden(schulz,meier).
 verschieden(schulz,mueller).
 verschieden(schulz,schroeder).
 
-keinedoppelten(V,S,K) :-
-    verschieden(V,S),
-    verschieden(V,K),
-    verschieden(S,K).
+keinedoppelten(V,S,K) :- verschieden(V,S),verschieden(V,K),verschieden(S,K).
 
 nichtMeier(mueller).
 nichtMeier(schroeder).
@@ -34,30 +31,30 @@ nichtSchulz(mueller).
 nichtSchulz(meier).
 nichtSchulz(schroeder).
 
-muellerimvorstand(V,S,K) :- V = mueller,kandidat(S),kandidat(K);
-                            S = mueller,kandidat(V),kandidat(K);
-                            K = mueller,kandidat(V),kandidat(S).
+muellerimvorstand(V,S,K) :- muellerimvorstand(mueller,kandidat(S),kandidat(K)).
+muellerimvorstand(V,S,K) :- muellerimvorstand(mueller,kandidat(V),kandidat(K)).
+muellerimvorstand(V,S,K) :- muellerimvorstand(mueller,kandidat(V),kandidat(S)).
 
-meierimvorstand(V,S,K) :- V = meier,kandidat(S),kandidat(K);
-                          S = meier,kandidat(V),kandidat(K);
-                          K = meier,kandidat(V),kandidat(S).
+meierimvorstand(V,S,K) :- meierimvorstand(meier,kandidat(S),kandidat(K)).
+meierimvorstand(V,S,K) :- meierimvorstand(meier,kandidat(V),kandidat(K)).
+meierimvorstand(V,S,K) :- meierimvorstand(meier,kandidat(V),kandidat(S)).
 
 meiernichtimvorstand(V,S,K) :- nichtMeier(V),nichtMeier(S),nichtMeier(K).
 
 problemEins(V,S,K) :- muellerimvorstand(V,S,K),nichtMeier(V),nichtMeier(S),nichtMeier(K).
 problemEins(V,S,K) :- meierimvorstand(V,S,K),nichtMueller(V),nichtMueller(S),nichtMueller(K).
 
-problemZwei(V,S,K) :- V = schulz, kandidat(S), kandidat(K).
+problemZwei(V,S,K) :- problemZwei(schulz,kandidat(S), kandidat(K)).
 problemZwei(V,S,K) :- nichtSchulz(V),nichtMueller(V),nichtMueller(S),nichtMueller(K).
 
 problemDrei(V,S,K) :- meierimvorstand(V,S,K),kandidat(V),kandidat(S),kandidat(K).
 problemDrei(V,S,K) :- meiernichtimvorstand(V,S,K),nichtSchroeder(V),nichtSchroeder(S),nichtSchroeder(K).
 
-problemVier(V,S,K) :- S = schulz,nichtMeier(V),nichtMeier(K).
+problemVier(V,S,K) :- problemVier(V,schulz,K),nichtMeier(V),nichtMeier(K).
 problemVier(V,S,K) :- nichtSchulz(S),kandidat(V),kandidat(K).
 
 problemFunf(V,S,K) :- nichtSchroeder(V),kandidat(S),kandidat(K).
-problemFunf(V,S,K) :- V = schroeder,nichtSchulz(S),nichtSchulz(K).
+problemFunf(V,S,K) :- problemFunf(schroeder,S,K),nichtSchulz(S),nichtSchulz(K).
 
 vorsitzender(V) :- kandidat(V).
 schriftfuehrer(S) :- kandidat(S).
