9bd1bc4b48b61ab2815142fdf7920376428e763b
Rico Klimpel <rico@tklimpel.de>
2018-03-06 08:55:25 +0100

    add PrettyWithVars to class Pretty

--- Pretty.hs
+++ Pretty.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Pretty where
 
 import Type
@@ -6,61 +8,69 @@ import Data.List
 
 class Pretty a where
     pretty :: a -> String
+    prettyWithVars :: [(VarIndex, String)] -> a -> String
 
--- macht Substitutionen lesbar
-instance Pretty Subst where
-    pretty (Subst r) = "sigma = {" ++ (listToString (map pretReplace r)) ++ "}"
-
--- kümmert sich um einzelne Replace Statements
-pretReplace :: Replace -> String
-pretReplace (Replace i t) = (pretty (Var i)) ++ " -> " ++ (pretty t)
+instance Pretty VarIndex where
+    pretty i = prettyWithVars [] i
+    prettyWithVars realNames i = case lookup i realNames of
+                                    Just n  -> n
+                                    Nothing -> prettyVarNames !! i
 
 -- macht Terme lesbar (gültige Prolog darstellung)
 instance Pretty Term where
-    pretty (Var x)               = prettyVarNames !! x
-    pretty (Comb s [])           = s
-    pretty (Comb "." (t1:t2))    = dot t1 (head t2)
-    pretty (Comb s terms)        = s ++ "(" ++ listToString(map pretty terms) ++ ")"
-
--- Verarbeitet die "." Operation
-dot :: Term -> Term -> String
-dot (Var x) (Comb "[]" [])         = "[" ++ (charToString (alphabet !! x)) ++ "]"
-dot (Var x) (Var y)                = "[" ++ (charToString (alphabet !! x)) ++ "|"
-                                         ++ (charToString (alphabet !! y)) ++ "]"
-
--- TODO Gianmarco Comments...                                         
-dot (Var x) (Comb "." [Comb c [], Comb "[]" []] ) = "[" ++  (pretty (Var x)) ++ ","  ++ c ++ "]"
-dot (Var x) (Comb "." (t1:t2))                    = "[" ++  (pretty (Var x)) ++ "|" ++ (dot t1 (head t2)) ++ "]"
--- konstante & variable
-dot (Comb c []) (Var x)                           = "[" ++ c ++ "|" ++ (pretty (Var x)) ++ "]" 
--- konstante & leere list -> konstante wird alleine in Liste geschrieben 
-dot (Comb c []) (Comb "[]" [])                    = "[" ++ c ++ "]"
-dot (Comb c []) (Comb "." (t1:t2))                = "[" ++ c ++ "," ++ (removeBrackets (dot t1 (head t2))) ++ "]"
-
-
+    pretty t = prettyWithVars [] t
+    prettyWithVars realNames (Var x)               = prettyWithVars realNames x
+    prettyWithVars realNames (Comb s [])           = s
+    prettyWithVars realNames (Comb "." (t1:t2))    = dot realNames t1 (head t2)
+        where
+        -- Verarbeitet die "." Operation
+        dot :: [(VarIndex,String)] -> Term -> Term -> String
+        dot realNames (Var x) (Comb "[]" [])                        = "[" ++ (prettyWithVars realNames x) ++ "]"
+        dot realNames (Var x) (Var y)                               = "[" ++ (prettyWithVars realNames x) ++ "|"
+                                                                            ++ (prettyWithVars realNames y) ++ "]"
+        -- TODO Gianmarco Comments...                                         
+        dot realNames (Var x) (Comb "." [Comb c [], Comb "[]" []] ) = "[" ++  (prettyWithVars realNames x) 
+                                                                            ++ ","  ++ c ++ "]"
+        dot realNames (Var x) (Comb "." (t1:t2))                    = "[" ++  (prettyWithVars realNames x)
+                                                                            ++ "|" ++ (dot realNames t1 (head t2)) ++ "]"
+        -- konstante & variable
+        dot realNames (Comb c []) (Var x)                           = "[" ++ c ++ "|" ++ (prettyWithVars realNames x) ++ "]" 
+        -- konstante & leere list -> konstante wird alleine in Liste geschrieben 
+        dot realNames (Comb c []) (Comb "[]" [])                    = "[" ++ c ++ "]"
+        dot realNames (Comb c []) (Comb "." (t1:t2))                = "[" ++ c ++ "," ++ (removeBrackets (dot realNames t1 (head t2))) ++ "]"
+    prettyWithVars realNames (Comb s terms)        = s ++ "(" ++ listToString(map (prettyWithVars realNames) terms) ++ ")"
 
 -- macht Rules lesbar
 instance Pretty Rule where
-    pretty (rh :- []) = pretty rh ++ "."
-    pretty (rh :- rt) = pretty rh ++ " :- " ++ (concat (intersperse "," (map pretty rt))) ++ "."
+    pretty r = prettyWithVars [] r
+    prettyWithVars realNames (rh :- []) = (prettyWithVars realNames rh) ++ "."
+    prettyWithVars realNames (rh :- rt) = (prettyWithVars realNames rh) ++ " :- " 
+                                          ++ (concat (intersperse "," (map (prettyWithVars realNames) rt))) ++ "."
 
 instance Pretty Goal where
-    pretty (Goal ts) = "?- " ++ (concat (intersperse ", " (map pretty ts))) ++ "."
+    pretty g = prettyWithVars [] g
+    prettyWithVars realNames (Goal ts) = "?- " ++ (concat (intersperse ", " (map (prettyWithVars realNames) ts))) ++ "."
 
 instance Pretty Prog where
-    pretty (Prog rs) = concat (intersperse "\n" (map pretty rs))
+    pretty p = prettyWithVars [] p
+    prettyWithVars realNames (Prog rs) = concat (intersperse "\n" (map (prettyWithVars realNames) rs))
+
+-- macht Substitutionen lesbar
+instance Pretty Subst where
+    pretty s = prettyWithVars [] s
+    prettyWithVars realNames (Subst r) = "sigma = {" ++ (listToString (map (pretReplace realNames) r)) ++ "}"
+        where
+        -- kümmert sich um einzelne Replace Statements
+        pretReplace :: [(VarIndex,String)] -> Replace -> String
+        pretReplace realNames (Replace i t) = (prettyWithVars realNames i) ++ " -> " ++ (prettyWithVars realNames t)
 
 instance Pretty SLDTree where
-    pretty (SLDTree (Goal ts) ledges) = (pretty (Goal ts)) ++ "\n" ++ (concatMap (prettyLedges 0) ledges)
+    pretty t = prettyWithVars [] t
+    prettyWithVars realNames (SLDTree (Goal ts) ledges) = (prettyWithVars realNames (Goal ts)) ++ "\n" ++ (concatMap (prettyLedges 0) ledges)
         where 
             prettyLedges k ((Subst rs),(SLDTree (Goal ts) ledges)) =   
-                                vertLines k ++ "+--" ++ pretty (Subst rs) ++ "\n"
-                                ++ vertLines (k+1) ++ pretty (Goal ts) ++ "\n"
+                                vertLines k ++ "+--" ++ (prettyWithVars realNames (Subst rs)) ++ "\n"
+                                ++ vertLines (k+1) ++ (prettyWithVars realNames (Goal ts)) ++ "\n"
                                 ++ concatMap (prettyLedges (k+1)) ledges
                 where
                     vertLines k = concat (take k (repeat "|   "))
-
-data Solution = Solution [Subst]
-
-instance Pretty Solution where
-    pretty (Solution subst) = concat ( intersperse "\n" (map pretty subst)) ++ "\n"
