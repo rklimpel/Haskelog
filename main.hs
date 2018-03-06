11374af2178cd1973f8b5c3c0738b5ade8fc0e44
rklimpel <rico@tklimpel.de>
2018-03-06 11:51:45 +0100

    add main.hs to load Tests and Interactive Prolog Shell at once

---
+++ main.hs
@@ -0,0 +1,17 @@
+module Main (main) where
+
+import qualified Interactive
+import Test.Test
+{-
+
+Welcome to Haskelog - Simple Prolog Interpreter written in Haskell.
+
+- Type main to Enter the Interactive Prolog Shell
+- Lookup Functions in /Test/Test.hs and use them to Test the Projekt
+
+Rico & Gianmarco @2018
+
+-}
+
+main :: IO()
+main = Interactive.main
