82c5e19b990f68a72bde6972ecbba4254e818b2d
Rico Klimpel <rico@tklimpel.de>
2018-03-09 13:24:49 +0100

    remove release folder :P ;D

--- release/map.pl
+++
@@ -1,4 +0,0 @@
-% Check if mapping the predicate P over the first list yields the second list
-maplist(_, [], []).
-maplist(P, [X|Xs], [Y|Ys]) :- call(P, X, Y), maplist(P, Xs, Ys).
-
--- release/sample.pl
+++
@@ -1,5 +0,0 @@
-% A simple Prolog program
-p(X, Z) :- q(X, Y), p(Y, Z).
-p(X, X).
-
-q(a, b).
